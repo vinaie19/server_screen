4b7213b36bf570f95f0cd466e92c94bd52c9d6fc
Vinay N <vinay@swipepages.com>
2023-01-31 04:17:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const { chromium } = require("playwright");
+const AWS = require("aws-sdk");
+require('dotenv').config()
+
+
+AWS.config.update({
+	accessKeyId: process.env.ACCESSKEYID,
+	secretAccessKey: process.env.SECRETACCESSKEY
+});
+const ep = new AWS.Endpoint("https://sgp1.digitaloceanspaces.com/");
+const s3 = new AWS.S3({ endpoint: ep });
+
+exports.s3 = s3;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/screenshot/variant/:variantId', (req, res) => {   
+    let newBrowser , newPage;
+    const url = req.body.url || "www.google.com"
+    chromium.launch()
+    .then(browser => {
+        newBrowser = browser
+        return browser.newPage()
+    })
+    .then((page) => {
+        newPage = page
+        return newPage.setViewportSize({ width: 1280, height: 1080 })
+    })
+    .then(() => newPage.goto(url, {waitUntil: "networkidle0"}))
+    .then(() => newPage.screenshot({type: "jpeg" }))
+    .then((screenshot) => {
+        const params = { Bucket: "sp-assets-staging", Key: `screenshots/variants/${req.params.variantId}`, Body: screenshot, ContentType: "image/jpeg", ACL: "public-read" }
+        return s3.upload(params).promise();
+    })
+    .then(() => newBrowser.close())
+    .then(() => res.status(201).json({type: "success"}))
+});
+
+app.listen(4000, () => console.log('server started'));
